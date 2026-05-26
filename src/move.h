ec660f5765e07a0b37969a92d1a88d2930d4b750
LiamEvers02 <liamevers14@gmail.com>
2026-05-26 21:25:17 +1200

    Create move.h

---
+++ src/move.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "types.h"
+
+struct Move {
+    uint16_t data;
+
+    constexpr Move() : data(0) {}
+    constexpr explicit Move(uint16_t d) : data(d) {}
+
+    constexpr bool operator==(Move m) const { return data == m.data;}
+    constexpr bool operator!=(Move m) const { return data != m.data;}
+
+};
+
+constexpr Move MOVE_NONE = Move(0);
+constexpr Move MOVE_NULL = Move(65); // encodes A1->B1, an impossible move used as a null-move sentinel in search
+
+constexpr Square from_sq(Move m){
+    return static_cast<Square>(m.data & 0x3F);
+}
+
+constexpr Square to_sq(Move m){
+    return static_cast<Square>((m.data >> 6) & 0x3F);
+}
+
+constexpr MoveType type_of(Move m) {
+    return static_cast<MoveType>(m.data & (3u << 14));
+}
+
+constexpr PieceType promotion_type(Move m) {
+    return static_cast<PieceType>(((m.data >> 12) & 3) + KNIGHT);
+} 
+
+constexpr bool is_ok(Move m){
+    return m != MOVE_NONE && m != MOVE_NULL;
+}
+
+template<MoveType T>
+constexpr Move make_move(Square from, Square to, PieceType pt = KNIGHT){
+    return Move(static_cast<uint16_t>(T | ((pt - KNIGHT) << 12) | (to << 6) | from));
+}
+
+constexpr Move make_move(Square from, Square to){
+    return make_move<NORMAL>(from, to);
+}
